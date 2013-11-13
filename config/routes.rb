3b80699c71aafdaffa985fc23679b77dbb34da69
Daniel Renner <renner.da@gmail.com>
2013-11-13 15:58:26 +0100

    Update routes.rb
    Use 2.1-stable with rails4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.prepend do
 
-  match "/email_to_friend/:type/:id" => 'email_sender#send_mail', :as => :email_to_friend
+  post "/email_to_friend/:type/:id" => 'email_sender#send_mail', :as => :email_to_friend
 
   namespace :admin do
     resource :captcha_settings
